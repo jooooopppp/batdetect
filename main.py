f88f8a2d92778b2f1a135008c271b16c21a1a0b9
jooooopppp <133588116+jooooopppp@users.noreply.github.com>
2023-07-07 12:15:39 +0100

    updated with new version code

--- main.py
+++ main.py
@@ -1,45 +1,124 @@
-from utils.preprocessing import read_wav_file, bandpass_filter, transform_data
-from utils.model_loading import load_model
-from utils.analysis import postprocess_output, visualize_results
-import torch
 import os
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from batdetect2 import api
+from batdetect2 import plot
+import glob
 
-# Load the pre-trained model
-model_path = 'models/Net2DFast_UK_same.pth.tar'
+MAX_DURATION = 2
+DETECTION_THRESHOLD = 0.3
 
-# Check if model file exists
-if not os.path.isfile(model_path):
-    raise FileNotFoundError(f"No model file found at {model_path}")
 
-model = load_model(model_path)  # Ensure this function correctly loads your model
+def make_prediction(file_name, detection_threshold=DETECTION_THRESHOLD):
+    # configure the model run
+    run_config = api.get_config(
+        detection_threshold=detection_threshold,
+        max_duration=MAX_DURATION,
+    )
 
-# Get a list of all the raw audio files in your 'data/raw' directory
-audio_files = [f for f in os.listdir('./data/raw') if f.endswith('.wav')]
+    # process the file to generate predictions
+    results = api.process_file(file_name, config=run_config)
 
-# Loop over each audio file
-for audio_file in audio_files:
-    # Read the audio file
-    try:
-        samplerate, data = read_wav_file(f'./data/raw/{audio_file}')
-    except Exception as e:
-        print(f"Error reading file {audio_file}: {e}")
-        continue
+    # extract the detections
+    detections = results["pred_dict"]["annotation"]
 
-    # Use the bandpass filter on your data
-    filtered_data = bandpass_filter(data, lowcut=20000, highcut=100000, fs=samplerate)
+    # create a dictionary to store the highest detection probabilities for each species
+    species_probs = {}
 
-    # Transform your filtered data to the correct format for the model
-    transformed_data = transform_data(filtered_data)  # Ensure this function correctly formats your data
+    # iterate over the detections and store the highest detection probability for each species
+    for pred in detections:
+        species = pred["class"]
+        detection_prob = pred["class_prob"]
+        if species in species_probs:
+            if detection_prob > species_probs[species]:
+                species_probs[species] = detection_prob
+        else:
+            species_probs[species] = detection_prob
 
-    # Now use the loaded model to make predictions
-    model.eval()  # Put the model in evaluation mode
-    with torch.no_grad():  # Don't calculate gradients for speed
-        predictions = model(transformed_data)
+    # create a dataframe of the predictions with the highest detection probabilities
+    df = pd.DataFrame(
+        [
+            {
+                "file_name": os.path.basename(file_name),
+                "time": pred["start_time"],
+                "species": species,
+                "detection_prob": detection_prob,
+            }
+            for species, detection_prob in species_probs.items()
+        ]
+    )
 
-    # Post-process the model's output
-    postprocessed_output = postprocess_output(predictions)  # Ensure this function correctly processes your predictions
+    im = generate_results_image(file_name, detections, run_config)
 
-    # Visualize the results
-    visualize_results(postprocessed_output)  # Ensure this function correctly visualizes your results
+    return im, df
 
-# ... rest of your analysis ...
+def generate_results_image(file_name, detections, config):
+    audio = api.load_audio(
+        file_name,
+        max_duration=config["max_duration"],
+        time_exp_fact=config["time_expansion"],
+        target_samp_rate=config["target_samp_rate"],
+    )
+
+    spec = api.generate_spectrogram(audio, config=config)
+
+    plt.close("all")
+    fig = plt.figure(
+        1,
+        figsize=(15, 4),
+        dpi=100,
+        frameon=False,
+    )
+    ax = fig.add_subplot(111)
+    plot.spectrogram_with_detections(spec, detections, ax=ax)
+    plt.tight_layout()
+
+    # Convert figure to numpy array
+    fig.canvas.draw()
+    im = np.array(fig.canvas.renderer.buffer_rgba())
+
+    return im
+
+def save_results_image(file_name, image):
+    output_folder = "data/output"
+    file_name_without_ext = os.path.splitext(os.path.basename(file_name))[0]
+    image_path = os.path.join(output_folder, file_name_without_ext + ".png")
+    image_path_greyscale = os.path.join(output_folder, file_name_without_ext + "_greyscale.png")
+
+    # Save the color image
+    plt.imsave(image_path, image)
+    print("Color image saved:", image_path)
+
+    # Convert the image to grayscale
+    image_greyscale = np.dot(image[..., :3], [0.2989, 0.5870, 0.1140])
+
+    # Save the grayscale image
+    plt.imsave(image_path_greyscale, image_greyscale, cmap='gray')
+    print("Grayscale image saved:", image_path_greyscale)
+
+def process_audio_files():
+    raw_folder = "data/raw"
+    output_folder = "data/output"
+    audio_files = glob.glob(os.path.join(raw_folder, "*.wav"))
+
+    df_list = []
+
+    for file_path in audio_files:
+        im, df = make_prediction(file_path)
+        save_results_image(file_path, im)
+
+        # Add the dataframe to the list
+        df_list.append(df)
+
+    # Concatenate all the dataframes
+    predictions_df = pd.concat(df_list, ignore_index=True)
+
+    # Save the predictions as CSV
+    predictions_path = os.path.join(output_folder, "predictions.csv")
+    predictions_df.to_csv(predictions_path, index=False)
+    print("Predictions saved:", predictions_path)
+
+
+if __name__ == "__main__":
+    process_audio_files()
