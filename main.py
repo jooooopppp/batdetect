ef14744441b52ba8a72d6b2bba2fed261cb4e79f
Nadeem P <pnadheem@gmail.com>
2023-07-28 18:07:02 +0100

    added new changes - requirements

--- main.py
+++ main.py
@@ -5,53 +5,11 @@ import pandas as pd
 from batdetect2 import api
 from batdetect2 import plot
 import glob
+import gc
 
 MAX_DURATION = 2
 DETECTION_THRESHOLD = 0.3
-
-
-def make_prediction(file_name, detection_threshold=DETECTION_THRESHOLD):
-    # configure the model run
-    run_config = api.get_config(
-        detection_threshold=detection_threshold,
-        max_duration=MAX_DURATION,
-    )
-
-    # process the file to generate predictions
-    results = api.process_file(file_name, config=run_config)
-
-    # extract the detections
-    detections = results["pred_dict"]["annotation"]
-
-    # create a dictionary to store the highest detection probabilities for each species
-    species_probs = {}
-
-    # iterate over the detections and store the highest detection probability for each species
-    for pred in detections:
-        species = pred["class"]
-        detection_prob = pred["class_prob"]
-        if species in species_probs:
-            if detection_prob > species_probs[species]:
-                species_probs[species] = detection_prob
-        else:
-            species_probs[species] = detection_prob
-
-    # create a dataframe of the predictions with the highest detection probabilities
-    df = pd.DataFrame(
-        [
-            {
-                "file_name": os.path.basename(file_name),
-                "time": pred["start_time"],
-                "species": species,
-                "detection_prob": detection_prob,
-            }
-            for species, detection_prob in species_probs.items()
-        ]
-    )
-
-    im = generate_results_image(file_name, detections, run_config)
-
-    return im, df
+BATCH_SIZE = 500
 
 def generate_results_image(file_name, detections, config):
     audio = api.load_audio(
@@ -74,11 +32,79 @@ def generate_results_image(file_name, detections, config):
     plot.spectrogram_with_detections(spec, detections, ax=ax)
     plt.tight_layout()
 
-    # Convert figure to numpy array
     fig.canvas.draw()
     im = np.array(fig.canvas.renderer.buffer_rgba())
 
-    return im
+    return im, spec
+
+def calculate_mean_frequency(spec):
+    spec_np = spec.cpu().numpy()  # Convert tensor to Numpy array
+
+    threshold = np.max(spec_np) * 0.1
+    mean_frequency = np.mean(np.where(spec_np > threshold)[0])
+    return mean_frequency
+
+def calculate_min_frequency(spec):
+    spec_np = spec.cpu().numpy()
+
+    threshold = np.max(spec_np) * 0.1
+    min_frequency = np.min(np.where(spec_np > threshold)[0])
+    return min_frequency
+
+def calculate_max_frequency(spec):
+    spec_np = spec.cpu().numpy()
+
+    threshold = np.max(spec_np) * 0.1
+    max_frequency = np.max(np.where(spec_np > threshold)[0])
+    return max_frequency
+
+def calculate_peak_intensity(spec):
+    spec_np = spec.cpu().numpy()
+
+    peak_intensity = np.max(spec_np)
+    return peak_intensity
+
+
+def make_prediction(file_name, detection_threshold=DETECTION_THRESHOLD):
+    run_config = api.get_config(
+        detection_threshold=detection_threshold,
+        max_duration=MAX_DURATION,
+    )
+
+    results = api.process_file(file_name, config=run_config)
+    detections = results["pred_dict"]["annotation"]
+
+    species_probs = {}
+    for pred in detections:
+        species = pred["class"]
+        detection_prob = pred["class_prob"]
+        if species in species_probs:
+            if detection_prob > species_probs[species]:
+                species_probs[species] = detection_prob
+        else:
+            species_probs[species] = detection_prob
+
+    im, spec = generate_results_image(file_name, detections, run_config)
+    df = pd.DataFrame(
+        [
+            {
+                "file_name": os.path.basename(file_name),
+                "time": pred["start_time"],
+                "species": species,
+                "detection_prob": detection_prob,
+                "mean_frequency": calculate_mean_frequency(spec),
+                "start_time": pred["start_time"],
+                "end_time": pred["end_time"],
+                "min_frequency": calculate_min_frequency(spec),
+                "max_frequency": calculate_max_frequency(spec),
+                "sampling_rate": run_config["target_samp_rate"],
+                "peak_intensity": calculate_peak_intensity(spec),
+            }
+            for species, detection_prob in species_probs.items()
+        ]
+    )
+
+    return im, df
 
 def save_results_image(file_name, image):
     output_folder = "data/output"
@@ -86,39 +112,38 @@ def save_results_image(file_name, image):
     image_path = os.path.join(output_folder, file_name_without_ext + ".png")
     image_path_greyscale = os.path.join(output_folder, file_name_without_ext + "_greyscale.png")
 
-    # Save the color image
+    os.makedirs(output_folder, exist_ok=True)
+
     plt.imsave(image_path, image)
     print("Color image saved:", image_path)
 
-    # Convert the image to grayscale
     image_greyscale = np.dot(image[..., :3], [0.2989, 0.5870, 0.1140])
-
-    # Save the grayscale image
     plt.imsave(image_path_greyscale, image_greyscale, cmap='gray')
     print("Grayscale image saved:", image_path_greyscale)
+
+    plt.close()
 
 def process_audio_files():
     raw_folder = "data/raw"
     output_folder = "data/output"
     audio_files = glob.glob(os.path.join(raw_folder, "*.wav"))
 
-    df_list = []
+    predictions_df = pd.DataFrame()
 
-    for file_path in audio_files:
-        im, df = make_prediction(file_path)
-        save_results_image(file_path, im)
+    for i in range(0, len(audio_files), BATCH_SIZE):
+        batch_files = audio_files[i:i+BATCH_SIZE]
 
-        # Add the dataframe to the list
-        df_list.append(df)
+        for file_path in batch_files:
+            im, df = make_prediction(file_path)
+            save_results_image(file_path, im)
 
-    # Concatenate all the dataframes
-    predictions_df = pd.concat(df_list, ignore_index=True)
+            predictions_df = pd.concat([predictions_df, df], ignore_index=True)
 
-    # Save the predictions as CSV
-    predictions_path = os.path.join(output_folder, "predictions.csv")
-    predictions_df.to_csv(predictions_path, index=False)
-    print("Predictions saved:", predictions_path)
+            predictions_path = os.path.join(output_folder, "predictions.csv")
+            predictions_df.to_csv(predictions_path, index=False)
+            print("Predictions saved:", predictions_path)
 
+        gc.collect()
 
 if __name__ == "__main__":
     process_audio_files()
